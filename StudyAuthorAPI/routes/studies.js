6a3d54f7e35a09ec567acde758ad1cdf2717fd39
Robert Day <day.c.robert@gmail.com>
2016-08-04 06:36:46 -0400

    adding crud to studies api

--- StudyAuthorAPI/routes/studies.js
+++ StudyAuthorAPI/routes/studies.js
@@ -9,10 +9,64 @@ var studies = [
     { id: 3, name: 'weather' }
 ];
 
-
-/* GET users listing. */
+//  get all studies
 router.get('/', function (req, res) {
     res.send(studies);
+});
+
+// get a study
+router.get('/:studyid', function (req, res) {
+    var studyid = req.params.studyid;
+    var ret = studies.find(function (r) {
+        return r.id == studyid;
+    });
+
+    if (!ret) {
+        res.sendStatus(404);
+    }
+
+    res.send(ret);
+});
+
+// update a study
+router.put('/:studyid', function (req, res) {
+    var studyid = req.params.studyid;
+    var ret = studies.find(function (r) {
+        return r.id == studyid;
+    });
+
+    if (!ret) {
+        res.sendStatus(404);
+    }
+
+    ret.name = req.body.name;
+    res.status(200).send(ret);
+});
+
+// Add a study
+router.post('/', function (req, res) {
+    var newStudy = {
+        id: 100,
+        name: req.body.name
+    };
+
+    studies.push(newStudy);
+    res.status(202).send(newStudy);
+});
+
+// delete a study
+router.delete('/:studyid', function (req, res) {
+    var studyid = req.params.studyid;
+
+    for (var i = 0; i < studies.length; ++i) {
+        if (studies[i].id == studyid) {
+            studies.splice(i, 1);
+            res.sendStatus(200);
+            return;
+        }
+    }
+
+    res.sendStatus(404);
 });
 
 module.exports = router;
