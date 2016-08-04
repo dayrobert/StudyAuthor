dbbf202ad74b9a45981166b65c3d74176df8002a
Robert Day <day.c.robert@gmail.com>
2016-08-04 05:54:28 -0400

    init

---
+++ StudyAuthor/typings/index.d.ts
@@ -0,0 +1,5 @@
+/// <reference path="globals/angular-ui-bootstrap/index.d.ts" />
+/// <reference path="globals/angular/index.d.ts" />
+/// <reference path="globals/express/index.d.ts" />
+/// <reference path="globals/jade/index.d.ts" />
+/// <reference path="globals/node/index.d.ts" />
---
+++ StudyAuthorAPI/routes/studies.js
@@ -0,0 +1,18 @@
+﻿var express = require('express');
+var router = express.Router();
+
+var mongo = require('mongoose');
+
+var studies = [
+    { id: 1, name: 'cars' },
+    { id: 2, name: 'birds' },
+    { id: 3, name: 'weather' }
+];
+
+
+/* GET users listing. */
+router.get('/', function (req, res) {
+    res.send(studies);
+});
+
+module.exports = router;
